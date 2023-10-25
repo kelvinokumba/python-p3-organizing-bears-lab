402291771abb711892b86c063f419adf63d242c1
Kelvin okumba <okumbakelvin@gmail.com>
2023-10-25 12:31:07 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+  ('Tabitha', 4, 'F', 'White', 'Calm', 1),
+  ('Sergeant Brown', 6, 'M', 'Brown', 'Grumpy', 1),
+  ('Melissa', 7, 'F', 'Red', 'Sweet', 0),
+  ('Grinch', 2, 'M', 'Green', 'Sly', 1),
+  ('Wendy', 3, 'F', 'Yellow', 'Energetic', 1),
+  ('unnamed', NULL, NULL, NULL, NULL, 1);
